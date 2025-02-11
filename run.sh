916d72148d8613f28127ee63dcaca2301bc723d2
MysteriousOyster <2168151@jeffcoschools.us>
2025-02-11 12:07:13 -0700

    more fix

--- run.sh
+++ run.sh
@@ -11,8 +11,7 @@ end=$(date +%s.%N)
 elapsed=$(echo "$end - $start" | bc)
 
 echo "Done compiling. Took $elapsed seconds."
-echo "Starting program..."
 
 echo -e "\e[0m"
 
-sudo ./main
+# sudo ./main
